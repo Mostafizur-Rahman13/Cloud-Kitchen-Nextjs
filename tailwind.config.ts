8cf15a61f6ad842b6b1599fefbd02f99e0ff2c3e
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-08-02 04:44:47 +0600

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      transparent: "transparent",
+      current:"currentColor",
+      white:"#ffffff",
+      color:"#000000",
+      lightgrey:"#444245",
+      grey:"rgba(0, 0, 0, 0.55)",
+      lightpink:"#F8F7F7",
+      darkpink:"#FEEDEA",
+      lightblack:"#333333",
+      textbl:"#363636",
+      footerlinks:"rgba(54, 54, 54, 0.9)",
+      bordertop:"rgba(105, 120, 131, 0.16)",
+      darkgrey:"rgba(44, 9, 11, 0.8)",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
