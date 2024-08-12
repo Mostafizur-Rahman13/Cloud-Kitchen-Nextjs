d887fd73788a01a9e486a83ba380a8ca01448d90
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-08-13 01:09:51 +0600

    Updated Cook Features

--- app/components/Work/index.tsx
+++ app/components/Work/index.tsx
@@ -1,7 +1,9 @@
+'use client'
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { Fade } from 'react-awesome-reveal'
 
 interface cardDataType {
   imgSrc: string;
@@ -41,35 +43,46 @@ const Work = () => {
   return (
     <div id='about-section' className='mx-auto max-w-7xl py-40 px-6'>
       <div className='text-center mb-14'>
-        <h3 className='text-pink text-lg font-normal mb-3 ls-51 uppercase'>Features</h3>
-        <p className='text-3xl lg:text-5xl font-semibold text-lightgrey'>
-          Get a many of interesting <br /> features.
-        </p>
-      </div>
-      <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-y-20 gap-x-5'>
-        {
-          cardData.map((items, i) => (
-            <div key={i}
-              className='card-b p-8 relative rounded-3xl'>
-              <div className='work-img rounded-full flex justify-center absolute top-[-50%] sm:top-[40%] md:top-[-55%] lg:top-[-45%] left-[0%]'>
-                <Image src={items.imgSrc} alt={items.imgSrc} width={510} height={10} />
-              </div>
-              <h3 className='text-2xl text-black font-semibold text-center mt-16 '>{items.heading}</h3>
-              <p className='text-lg font-normal text-black text-center text-opacity-50'>{items.subheading}</p>
 
-              <div className='flex items-center justify-center'>
-                <Link href='/'>
-                  <p className='flex items-center text-center text-lg font-medium text-pink mt-2 hover:underline'>
-                    {items.link}<ChevronRightIcon width={20} height={20} />
-                  </p>
-                </Link>
+        <Fade direction='up' delay={400} cascade damping={1e-1} triggerOnce={true}>
+          <h3 className='text-pink text-lg font-normal mb-3 ls-51 
+          uppercase'>Features</h3>
+        </Fade>
 
-              </div>
-            </div>
-          ))
-        }
+        <Fade direction='up' delay={800} cascade damping={1e-1} triggerOnce={true}>
+          <p className='text-3xl lg:text-5xl font-semibold text-lightgrey'>
+            Get a many of interesting <br /> features.
+          </p>
+        </Fade>
+
       </div>
-    </div>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-y-20 gap-x-5 mt-32'>
+
+        <Fade direction='up' delay={1000} cascade damping={1e-1} triggerOnce={true}>
+          {
+            cardData.map((items, i) => (
+              <div key={i}
+                className='card-b p-8 relative rounded-3xl'>
+                <div className='work-img rounded-full flex justify-center absolute top-[-50%] sm:top-[40%] md:top-[-55%] lg:top-[-45%] left-[0%]'>
+                  <Image src={items.imgSrc} alt={items.imgSrc} width={510} height={10} />
+                </div>
+                <h3 className='text-2xl text-black font-semibold text-center mt-16 '>{items.heading}</h3>
+                <p className='text-lg font-normal text-black text-center text-opacity-50'>{items.subheading}</p>
+
+                <div className='flex items-center justify-center'>
+                  <Link href='/'>
+                    <p className='flex items-center text-center text-lg font-medium text-pink mt-2 hover:underline'>
+                      {items.link}<ChevronRightIcon width={20} height={20} />
+                    </p>
+                  </Link>
+
+                </div>
+              </div>
+            ))
+          }
+        </Fade>
+      </div>
+    </div >
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main>
       <Banner />
       <Work />
-      <Features />
+      {/* <Features /> */}
       <Cook />
       <Expert />
       <Gallery />
