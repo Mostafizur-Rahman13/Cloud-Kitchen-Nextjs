c4a530adb32e31705487dbbe3124623161a56868
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-08-15 01:03:32 +0600

    Updated Slider

--- app/components/Cook/index.tsx
+++ app/components/Cook/index.tsx
@@ -3,41 +3,51 @@ import Image from 'next/image';
 import React from 'react';
 import CookImg from '@/public/Images/Cook/cook.png';
 import Burger from '@/public/Images/Cook/burger.png';
-
 import { Fade } from 'react-awesome-reveal';
 
 const Cook = () => {
   return (
     <div id='cook-section' className='relative'>
       <div className='mx-auto max-w-7xl lg:pt-20 sm:pb-24 px-6'>
-
         <div className='absolute right-0 bottom-[-18%] hidden lg:block'>
-          <Image src={Burger} alt={Burger} width={463} height={622} />
+          <Fade direction='right' delay={400} cascade damping={1e-1} triggerOnce={true}>
+            <Image src={Burger} alt={Burger} width={463} height={622} />
+          </Fade>
         </div>
+
         <div className='grid grid-cols-1 lg:grid-cols-12 my-16 space-x-5'>
           {/* Left Section */}
           <div className='col-span-6 flex justify-start'>
-            <Image src={CookImg} alt={CookImg} width={635} height={808} />
+            <Fade direction='left' delay={400} cascade damping={1e-1} triggerOnce={true}>
+              <Image src={CookImg} alt={CookImg} width={635} height={808} />
+            </Fade>
           </div>
-        </div>
 
+          {/* Right Section */}
+          <div className='col-span-6 flex flex-col justify-center'>
 
+            <Fade direction='up' delay={400} cascade damping={1e-1} triggerOnce={true}>
+              <h2 className='text-pink text-lg font-normal mb-3 ls-51 uppercase'>Cook with us</h2>
+            </Fade>
 
-        {/* Right Section */}
-        <div className='col-span-6 flex flex-col justify-center'>
+            <Fade direction='up' delay={800} cascade damping={1e-1} triggerOnce={true}>
+              <h3 className='text-3xl lg:text-5xl font-semibold text-black text-start'>Cooking together with the expert</h3>
+            </Fade>
 
-          <h2 className='text-pink text-lg font-normal mb-3 ls-51 uppercase'>Cook with us</h2>
-          <h3 className='text-3xl lg:text-5xl font-semibold text-black text-start'>Cooking together with the expert</h3>
-          <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus saepe architecto vel sit quibusdam ad cum qui similique quaerat obcaecati ipsam sapiente nesciunt enim deserunt corrupti blanditiis assumenda, soluta eum!Voluptates cupiditate vero explicabo cumque doloremque ab, quas dolor, dolore mollitia quia veniam commodi quo harum illum iure atque.
-          </p>
-          <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis at qui possimus ut nesciunt.....</p>
+            <Fade direction='down' delay={1000} cascade damping={1e-1} triggerOnce={true}>
+              <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus saepe architecto vel sit quibusdam ad cum qui similique quaerat obcaecati ipsam sapiente nesciunt enim deserunt corrupti blanditiis assumenda, soluta eum!Voluptates cupiditate vero explicabo cumque doloremque ab, quas dolor, dolore mollitia quia veniam commodi quo harum illum iure atque.
+              </p>
+              <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis at qui possimus ut nesciunt.....</p>
 
-          <div className='flex align-middle justify-center md:justify-start'>
-            <button className='text-xl justify-center bg-pink rounded-full text-white py-5 px-6 lg:px-10'>learn more</button>
+              <div className='flex align-middle justify-center md:justify-start'>
+                <button className='text-xl justify-center bg-pink rounded-full text-white py-5 px-6 lg:px-10'>learn more</button>
+              </div>
+
+            </Fade>
           </div>
-        </div>
+        </div >
       </div>
-    </div>
+    </div >
   )
 }
 
--- app/components/Expert/index.tsx
+++ app/components/Expert/index.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
+'use client'
+import React, { Component } from 'react';
+import Slider from 'react-slick'
+import Image from 'next/image';
+import Linkdin from '@/public/Images/Expert/Linkedin.svg'
 
-const Expert = () => {
-  return (
-    <div>Expert</div>
-  )
+// CAROUSEL DATA
+interface DataType {
+  profession: string;
+  name: string;
+  imgSrc: string;
 }
 
-export default Expert
+const postData: DataType[] = [
+  {
+    profession: 'Senior Chef',
+    name: 'Kevin Steve',
+    imgSrc: '/images/Expert/boyone.svg'
+  },
+  {
+    profession: 'Junior Chef',
+    name: 'Jamela',
+    imgSrc: '/images/Expert/girl.png'
+  },
+  {
+    profession: 'Junior Chef',
+    name: 'Barbara',
+    imgSrc: '/images/Expert/boytwo.svg'
+  },
+  {
+    profession: 'Junior Chef',
+    name: 'Egbat',
+    imgSrc: '/images/Expert/girl.png'
+  },
+  {
+    profession: 'Junior Chef',
+    name: 'Jesse',
+    imgSrc: '/images/Expert/girl.png'
+  },
+  {
+    profession: 'Junior Chef',
+    name: 'Lizanka',
+    imgSrc: '/images/Expert/girl.png'
+  },
+]
+
+// CAROUSEL SETTINGS
+
+export default class MultipleItems extends Component {
+  render() {
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      cssEase: 'liner',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToshow: 3,
+            SlidesToScroll: 1,
+            Infinity: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToshow: 2,
+            SlidesToScroll: 1,
+            Infinity: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToshow: 1,
+            SlidesToScroll: 1,
+            Infinity: true,
+            dots: true
+          }
+        },
+      ]
+    };
+
+    return (
+      <div className='py-10 sm:py-20 bg-darkpink '>
+        <div className='mx-auto max-2xl lg:max-w-7xl sm:py-4 lg:px-8'>
+          <div className='text-center'>
+            <h2 className='text-pink text-lg font-normal mb-3 tracking-widest uppercase ls-51'>Expert Chefs</h2>
+            <h3 className='text-3xl lg:text-5xl font-semibold text-black'>Let&apos;s meet the expert</h3>
+          </div>
+
+          <Slider {...settings}>
+            {
+              postData.map((items, i) => (
+                <div key={i}>
+                  <div>
+                    <div>
+                      <Image src={items.imgSrc} alt='Expert' width={362} height={262} className='inline-block relative m-auto' />
+
+                      <div className='absolute top-[40%]'>
+                        <Image src={Linkdin} alt={Linkdin} width={220} height={120} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </Slider>
+        </div>
+      </div>
+    )
+  }
+}
+
